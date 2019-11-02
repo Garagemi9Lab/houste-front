1292cf8fa94d78324eb8f215f20983b0c6ce6c42
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-11-02 17:24:00 -0300

    input footer added

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react'
+import style from './style'
+import { withStyles, TextField, IconButton } from '@material-ui/core'
+import { Send } from '@material-ui/icons'
 
 class Chat extends Component {
 
+    constructor(props) {
+        super(props)
+        this.messagesHolder = React.createRef()
+        this.inputTextField = React.createRef()
+    }
+
+    state = {
+        inputValue: ''
+    }
+
+    handleSubmit = e => {
+
+        e.preventDefault()
+        console.log('Form submitted')
+    }
+
+    handleInputChange = event => {
+        let value = event.target.value
+        this.setState({ inputValue: value })
+    }
+
     render() {
-        return <div>
-            Hello from {this.props.place}
-        </div>
+
+        const { classes } = this.props
+
+        return (
+            <div className={classes.holder}>
+                <div className={classes.messagesHolder} ref={this.messagesHolder}>
+                    <div>test</div>
+                </div>
+                <div className={classes.footer}>
+                    <form noValidate autoComplete="off" className={classes.footerHolder} onSubmit={this.handleSubmit}>
+                        <TextField
+                            id="input"
+                            className={classes.inputHolder}
+                            inputProps={{
+                                className: classes.inputTextField
+                            }}
+                            value={this.state.inputValue}
+                            margin="normal"
+                            placeholder="Informe a sua mensagem"
+                            onChange={this.handleInputChange}
+                            disabled={false}
+                            inputRef={this.inputTextField}
+                        />
+                        <IconButton color="primary" type="submit">
+                            <Send />
+                        </IconButton>
+                    </form>
+                </div>
+            </div>
+        )
     }
 
 }
 
-export default Chat
+export default withStyles(style)(Chat)
---
+++ src/components/Chat/style.js
@@ -0,0 +1,29 @@
+export default {
+    holder: {
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        overflowY: 'hidden'
+    },
+    messagesHolder: {
+        display: 'flex',
+        flexDirection: 'column',
+        overflowY: 'scroll',
+        width: '100%',
+        flex: '1 0'
+    },
+
+    footer: {
+
+    },
+    footerHolder: {
+        display: 'flex',
+    },
+
+    inputHolder: {
+        flexGrow: 1
+    },
+    inputTextField: {
+        paddingLeft: 5
+    }
+}
