724e3fc702829c9cac7c1eb92f84fc978c71f640
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-11-02 19:01:31 -0300

    configs added

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+import _env from './env'
+import { _headers } from './apis'
+import { _errorDetail } from './errors'
+
+import _theme from './theme'
+
+export const env = _env
+export const headers = _headers
+export const errorDetail = _errorDetail
+export const theme = _theme
